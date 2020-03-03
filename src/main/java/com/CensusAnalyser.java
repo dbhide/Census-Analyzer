0ff812bdf69478b0dc1ea4355adba00982ebaa46
dbhide <dhanashree.bhide3@gmail.com>
2020-03-03 17:50:34 +0530

    REFACTORED: Handled DRY violation at Iterator count

--- src/main/java/com/CensusAnalyser.java
+++ src/main/java/com/CensusAnalyser.java
@@ -2,24 +2,21 @@ package com;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.InputMismatchException;
 import java.util.Iterator;
-import java.util.concurrent.CompletionException;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try ( Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
-            Iterator<IndiaCensusCSV> stateCSVIterator = this.getCSVIterator(reader, IndiaCensusCSV.class);
-            Iterable<IndiaCensusCSV> csvIterable = () -> stateCSVIterator;
-            int namOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-            return namOfEntries;
+            Iterator<IndiaCensusCSV> censusCSVIterator = this.getCSVIterator(reader, IndiaCensusCSV.class);
+//            Iterable<IndiaCensusCSV> csvIterable = () -> stateCSVIterator;
+//            int namOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+//            return namOfEntries;
+            return getCount(censusCSVIterator);
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -41,13 +38,16 @@ public class CensusAnalyser {
         return stateCSVIterator;
     }
 
+    private <E> int getCount(Iterator<E> iterator) {
+        Iterable<E> csvIterable = () -> iterator;
+        int namOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+        return namOfEntries;
+    }
 
     public int loadIndiaStateCode(String csvFilePath) throws CensusAnalyserException {
         try ( Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
-            Iterator<IndiaStateCodeCSV> censusCSVIterator = this.getCSVIterator(reader, IndiaStateCodeCSV.class);
-            Iterable<IndiaStateCodeCSV> csvIterable = () -> censusCSVIterator;
-            int namOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-            return namOfEntries;
+            Iterator<IndiaStateCodeCSV> stateCSVIterator = this.getCSVIterator(reader, IndiaStateCodeCSV.class);
+            return getCount(stateCSVIterator);
         }
         catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
